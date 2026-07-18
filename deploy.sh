d1f0bdfe3d74cbcbaec7a268ca3599f426b65fab
alexw16 <alexw16@users.noreply.github.com>
2026-07-18 03:32:45 +0000

    Deploying to gh-pages from @ alexw16/alexw16.github.io@b8cd2169ec33df7a5b8c4db5a8448fca7c289fb5 🚀

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,11 @@ bundle exec jekyll build
 # Switch to the gh-pages branch
 git checkout gh-pages
 
-touch .nojekyll
+# Move the built site to the root, removing stale files (old source dirs,
+# previously built pages, etc.) that are no longer part of the build
+rsync -avr --delete --exclude='.git' --exclude='deploy.sh' --exclude='.nojekyll' _site/ .
 
-# Move the built site to the root
-rsync -avr --exclude='.git' --exclude='deploy.sh' _site/ .
+touch .nojekyll
 
 # Add, commit, and push the changes
 git add --all
